b504c4283d9bba3c4572464f175da18a4b03c40c
ellinakatrish <92501105+ellinakatrish@users.noreply.github.com>
2024-04-29 15:29:15 +0500

    logim form and controllers are ready

--- app/settings.py
+++ app/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-from django.conf.global_settings import MEDIA_ROOT, MEDIA_URL, STATICFILES_DIRS
+from django.conf.global_settings import AUTH_USER_MODEL, MEDIA_ROOT, MEDIA_URL, STATICFILES_DIRS
 from django.forms import Media
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -89,9 +89,9 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': '1324',
+        'NAME': 'home',
+        'USER': 'home',
+        'PASSWORD': 'home',
         'HOST': 'localhost',
         'PORT': '5432',         
     }
@@ -158,3 +158,4 @@ INTERNAL_IPS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'users.User'
--- users/views.py
+++ users/views.py
@@ -1,12 +1,30 @@
+from audioop import reverse
+from django.contrib import auth
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+
 from users.forms import UserLoginForm
 
-def login (request):
+def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+    
+            if user:
+                auth.login(request, user)               
+                return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = UserLoginForm()
+
     context = {
-        
-        'title':'Home - Authorization',
-    }       
+        'title': 'Home - authprization',
+        'form': form
+    }
     return render(request, 'users/login.html', context)
+
 
 
 def registration (request):
