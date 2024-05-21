1ae59ebf4594518c2e9e48747a4aeec211ea419a
ellinakatrish <92501105+ellinakatrish@users.noreply.github.com>
2024-05-22 00:11:12 +0500

    Add to cart and json reponce ( notifications bugs )

--- static/deps/js/jquery-ajax.js
+++ static/deps/js/jquery-ajax.js
@@ -3,53 +3,53 @@ $(document).ready(function () {
     //taking the markup element with id jq-notification for ajax notifications into a variable
     var successMessage = $("#jq-notification");
 
-    // //catching the click event on the add to cart button
-    // $(document).on("click", ".add-to-cart", function (e) {
-    //     //blocking its basic action
-    //     e.preventDefault();
+    //catching the click event on the add to cart button
+    $(document).on("click", ".add-to-cart", function (e) {
+        //blocking its basic action
+        e.preventDefault();
 
-    //     //taking the counter element in the cart icon and take the value from there
-    //     var goodsInCartCount = $("#goods-in-cart-count");
-    //     var cartCount = parseInt(goodsInCartCount.text() || 0);
+        //taking the counter element in the cart icon and take the value from there
+        var goodsInCartCount = $("#goods-in-cart-count");
+        var cartCount = parseInt(goodsInCartCount.text() || 0);
 
-    //     //getting the product id from the attribute data-product-id
-    //     var product_id = $(this).data("product-id");
+        //getting the product id from the attribute data-product-id
+        var product_id = $(this).data("product-id");
 
-    //     //from the href attribute we take a link to the django controller
-    //     var add_to_cart_url = $(this).attr("href");
+        //from the href attribute we take a link to the django controller
+        var add_to_cart_url = $(this).attr("href");
 
-    //     //making a post request via ajax without reloading the page
-    //     $.ajax({
-    //         type: "POST",
-    //         url: add_to_cart_url,
-    //         data: {
-    //             product_id: product_id,
-    //             csrfmiddlewaretoken: $("[name=csrfmiddlewaretoken]").val(),
-    //         },
-    //         success: function (data) {
-    //             // Messages
-    //             successMessage.html(data.message);
-    //             successMessage.fadeIn(400);
-    //             //In 7 seconds the message is gone
-    //             setTimeout(function () {
-    //                 successMessage.fadeOut(400);
-    //             }, 7000);
+        //making a post request via ajax without reloading the page
+        $.ajax({
+            type: "POST",
+            url: add_to_cart_url,
+            data: {
+                product_id: product_id,
+                csrfmiddlewaretoken: $("[name=csrfmiddlewaretoken]").val(),
+            },
+            success: function (data) {
+                // Messages
+                successMessage.html(data.message);
+                successMessage.fadeIn(400);
+                //In 7 seconds the message is gone
+                setTimeout(function () {
+                    successMessage.fadeOut(400);
+                }, 7000);
 
-    //             //increasing the number of items in the cart (rendered in the template)
-    //             cartCount++;
-    //             goodsInCartCount.text(cartCount);
+                //increasing the number of items in the cart (rendered in the template)
+                cartCount++;
+                goodsInCartCount.text(cartCount);
 
-    //             //changing the contents of the cart to the response from django (a new rendered fragment of the cart markup)
-    //             var cartItemsContainer = $("#cart-items-container");
-    //             cartItemsContainer.html(data.cart_items_html);
+                //changing the contents of the cart to the response from django (a new rendered fragment of the cart markup)
+                var cartItemsContainer = $("#cart-items-container");
+                cartItemsContainer.html(data.cart_items_html);
 
-    //         },
+            },
 
-    //         error: function (data) {
-    //             console.log("Error adding item to cart");
-    //         },
-    //     });
-    // });
+            error: function (data) {
+                console.log("Error adding item to cart");
+            },
+        });
+    });
 
 
 
@@ -67,7 +67,7 @@ $(document).ready(function () {
     //     var cart_id = $(this).data("cart-id");
     //     //from the href attribute we take a link to the controller django
     //     var remove_from_cart = $(this).attr("href");
-    
+
     //     // making a post request via ajax without reloading the page
     //     $.ajax({
 
@@ -159,17 +159,17 @@ $(document).ready(function () {
     //             quantity: quantity,
     //             csrfmiddlewaretoken: $("[name=csrfmiddlewaretoken]").val(),
     //         },
- 
+
     //         success: function (data) {
-    //              // messages
+    //             // messages
     //             successMessage.html(data.message);
     //             successMessage.fadeIn(400);
 
-    //              // In 7seconds message should gone
+    //             // In 7seconds message should gone
     //             setTimeout(function () {
-    //                  successMessage.fadeOut(400);
+    //                 successMessage.fadeOut(400);
     //             }, 7000);
- 
+
     //             //changing quantity of goods in basket
     //             var goodsInCartCount = $("#goods-in-cart-count");
     //             var cartCount = parseInt(goodsInCartCount.text() || 0);
